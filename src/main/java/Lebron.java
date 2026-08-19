6f2f2f0b70ee1a9c1c387a305016db673cc45ff3
Sean Tan <sntan1412nus@gmail.com>
2026-08-20 03:06:03 +0800

    Complete Level-1

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Lebron {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Lebron");
         System.out.println("What can I do for you?");
-        System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = scanner.nextLine();
+        }
+
+        System.out.println("That's game. See you next time!");
     }
 }
