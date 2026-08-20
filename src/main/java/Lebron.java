c93b700d55d9e8dc9afb3e608b14e3dc1bbbe396
Sean Tan <sntan1412nus@gmail.com>
2026-08-20 14:56:56 +0800

    Complete Level-3

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -4,7 +4,7 @@ public class Lebron {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println("Hello! I'm Lebron");
@@ -14,12 +14,23 @@ public class Lebron {
 
         while (!input.equals("bye")) {
 
-            if (input.equals("list")) {
+            if (input.equals("lipaidst")) {
+
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
+
+            } else if (input.startsWith("mark ")) {
+
+                int taskNumber = Integer.parseInt(input.substring(5));
+                tasks[taskNumber - 1].markAsDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[taskNumber - 1]);
+
             } else {
-                tasks[taskCount] = input;
+
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+}
