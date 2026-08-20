826677676a00d85cf8fc38df93908bbbb70e31c4
Sean Tan <sntan1412nus@gmail.com>
2026-08-20 13:55:27 +0800

    Complete Level-2

--- src/main/java/Lebron.java
+++ src/main/java/Lebron.java
@@ -4,13 +4,26 @@ public class Lebron {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("Hello! I'm Lebron");
         System.out.println("What can I do for you?");
 
         String input = scanner.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println(input);
+
+            if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
+            }
+
             input = scanner.nextLine();
         }
 
