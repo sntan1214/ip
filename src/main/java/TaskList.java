09341f26c2e6f02bdf730f39ecbc8040bd7ffcc3
Sean Tan <sntan1412nus@gmail.com>
2026-08-27 18:24:16 +0800

    Refactor chatbot into OOP components

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,43 @@
+public class TaskList {
+
+    private final Task[] tasks;
+    private int taskCount;
+
+    public TaskList() {
+        this.tasks = new Task[100];
+        this.taskCount = 0;
+    }
+
+    public void add(Task task) {
+        tasks[taskCount] = task;
+        taskCount++;
+    }
+
+    public Task get(int index) {
+        return tasks[index];
+    }
+
+    public int size() {
+        return taskCount;
+    }
+
+    public Task mark(int taskNumber) {
+        Task task = tasks[taskNumber - 1];
+        task.markAsDone();
+        return task;
+    }
+
+    public Task delete(int taskNumber) {
+        int index = taskNumber - 1;
+        Task deletedTask = tasks[index];
+
+        for (int i = index; i < taskCount - 1; i++) {
+            tasks[i] = tasks[i + 1];
+        }
+
+        tasks[taskCount - 1] = null;
+        taskCount--;
+
+        return deletedTask;
+    }
+}
